52e828b9f2b18010092b71879e8c76bcca79f713
peter leeh <leeh123peter@gmail.com>
2023-05-22 11:53:10 +0200

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -29,17 +29,17 @@ use std::panic::PanicInfo;
 /// # Backtrace
 ///
 /// The hook currently doesn't try to capture a backtrace.
-/// 
+///
 /// # Preserving previous hook
-/// 
-/// Sometimes it's desirable to preserve the previous panic hook, because other crates 
+///
+/// Sometimes it's desirable to preserve the previous panic hook, because other crates
 /// might rely on their panic hook integration to function properly.
-/// 
+///
 /// For this behavior, you can do the following:
-/// 
+///
 /// ```rust
 /// use tracing_panic::panic_hook;
-/// 
+///
 /// # #[allow(clippy::needless_doctest_main)]
 /// fn main() {
 ///     let prev_hook = std::panic::take_hook();
